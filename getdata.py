acfc0f1dccd8870a38d829d3d069a63fc23b0bb9
Paul Cuciureanu <paulcu@gmail.com>
2015-04-24 00:00:50 -0400

    Updated data download script with URL

--- getdata.py
+++ getdata.py
@@ -1,5 +1,3 @@
 #!/usr/bin/env python
 import urllib
-urllib.urlretrieve("https://www.kaggle.com/c/bioresponse/download/train.csv", filename="data/train.csv")
-urllib.urlretrieve("https://www.kaggle.com/c/bioresponse/download/test.csv", filename="data/test.csv")
-urllib.urlretrieve("https://www.kaggle.com/c/bioresponse/download/svm_benchmark.csv", filename="data/svm_benchmark.csv")
+print "Download Biological Response data from: https://www.kaggle.com/c/bioresponse/data"
